95a87323b440fe2bb71d6b2d5ffe082b820ea039
Grzegorz Matuszak <grzegorz-matuszak@wp.pl>
2020-05-09 09:34:18 +0200

    Init Project

---
+++ app/src/main/java/com/example/tuner/TuneList.kt
@@ -0,0 +1,12 @@
+package com.example.tuner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TuneList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tune_list)
+    }
+}
