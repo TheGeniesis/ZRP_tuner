62e597b3102f8bad27108b9a0f138aff5dfe7b8d
itsmkln <58135542+itsmkln@users.noreply.github.com>
2020-06-17 15:43:11 +0200

    edit1
    designing main screen, top bar disabled, to do: adjust design to all mobiles

--- app/src/main/java/com/example/tuner/MainActivity.kt
+++ app/src/main/java/com/example/tuner/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_title_bar.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         setTuneTexts(list)
 
 
-        title_bar.text = "SmoothTuner";
+
         button.setOnClickListener {
             val intent = Intent(this, TuneList::class.java)
             startActivity(intent)
